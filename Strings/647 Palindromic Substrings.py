16c16e00b684417fcf9f5b6641c6de68183bd115
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-08-23 14:59:46 +0530

    Update 647 Palindromic Substrings.py

--- Strings/647 Palindromic Substrings.py
+++ Strings/647 Palindromic Substrings.py
@@ -1,3 +1,5 @@
+## ITERATIVE
+
 class Solution:
     def countSubstrings(self, s: str) -> int:
         res=0
@@ -13,3 +15,24 @@ class Solution:
                 left-=1
                 right+=1
         return res
+
+    
+## RECURSIVE
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res=0
+        
+        def helper(s,left,right):
+            res=0
+            while left >=0 and right < len(s) and s[left]==s[right]:
+                res +=1
+                left-=1
+                right+=1
+            return res
+        
+        for i in range(len(s)):
+            res+=helper(s,i,i)
+            res+=helper(s,i,i+1)
+        return res
+    
