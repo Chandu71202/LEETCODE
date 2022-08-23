58e3eda4605e6649d593dbaf4adbf2e5bdd1b7ba
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-08-23 14:39:41 +0530

    Create 647 Palindromic Substrings.py

---
+++ Strings/647 Palindromic Substrings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res=0
+        for i in range(len(s)):
+            left , right = i , i
+            while left >=0 and right < len(s) and s[left]==s[right]:
+                res +=1
+                left-=1
+                right+=1
+            left , right = i , i+1
+            while left >=0 and right < len(s) and s[left]==s[right]:
+                res+=1
+                left-=1
+                right+=1
+        return res
