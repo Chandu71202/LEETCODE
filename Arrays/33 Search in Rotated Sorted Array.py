6efeafd81428cf854898098746420564ec3ddfff
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-09-05 21:24:47 +0530

    Update 33 Search in Rotated Sorted Array.py

--- Arrays/33 Search in Rotated Sorted Array.py
+++ Arrays/33 Search in Rotated Sorted Array.py
@@ -1,23 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start, end = 0, len(nums) - 1
-       
-        while start <= end:
-            mid = (start+end)//2
-            
-            if nums[mid] == target:
-                return mid
-            
-            elif nums[mid]<target:
-                if nums[start]<= target and nums[start] > nums[mid]:
-                    end = mid -1
-                else:
-                    start = mid+1
-            
-            elif nums[mid]>target:
-                if target<=nums[end] and nums[end]<nums[mid]:
-                    start = mid+1
-                else:
-                    end = mid -1 
+        try:
+            return nums.index(target)
+        except:
+            return -1
         
-        return -1
+#         start, end = 0, len(nums) - 1
+       
+#         while start <= end:
+#             mid = (start+end)//2
+            
+#             if nums[mid] == target:
+#                 return mid
+            
+#             elif nums[mid]<target:
+#                 if nums[start]<= target and nums[start] > nums[mid]:
+#                     end = mid -1
+#                 else:
+#                     start = mid+1
+            
+#             elif nums[mid]>target:
+#                 if target<=nums[end] and nums[end]<nums[mid]:
+#                     start = mid+1
+#                 else:
+#                     end = mid -1 
+        
+#         return -1
