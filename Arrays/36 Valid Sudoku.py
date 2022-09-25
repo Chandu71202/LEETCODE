f92b6d8bcb6be1ba2b277466264daa4fd8bb8ef0
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-09-25 22:18:45 +0530

    Update 36 Valid Sudoku.py

--- Arrays/36 Valid Sudoku.py
+++ Arrays/36 Valid Sudoku.py
@@ -27,5 +27,27 @@ class Solution:
                     if a!='.' and l.count(a)>1:
                         return False 
         return True
+    
+#     for i in range(9):
+#             for j in set(board[i]):
+#                 if j!='.' and board[i].count(j)>1:
+#                     return False
+                
+#         for i in range(9):
+#             l=[board[q][i] for q in range(9)]
+#             for j in set(l):
+#                 if j!='.' and l.count(j)>1:
+#                     return False
+                
+#         for i in range(0,9,3):
+#             for j in range(0,9,3):
+#                 l1 = [[board[p][q] for q in range(j,j+3)] for p in range(i,i+3)]
+#                 l=[]
+#                 for c in l1:
+#                     l+=c
+#                 for k in set(l):
+#                     if k!='.' and l.count(k)>1:
+#                         return False
+#         return True
                                  
                 
