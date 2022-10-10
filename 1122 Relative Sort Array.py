542e92da4f5dae5942c49879e3391b630c79d0f9
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-10-10 21:25:02 +0530

    Create 1122 Relative Sort Array.py

---
+++ 1122 Relative Sort Array.py
@@ -0,0 +1,33 @@
+class Solution:
+#   T.C = 103ms
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        c = defaultdict(int)
+        ans = []
+        left = []
+        for num in arr2:
+            if num in arr1:
+                c[num]=arr1.count(num)
+        
+        for key,value in c.items():
+            ans+=[key]*value
+            for _ in range(value):
+                arr1.remove(key)
+        return ans+sorted(arr1)
+        
+   
+# T.C = 105ms
+# class Solution:
+#     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+#         l = []
+#         for i in arr1:
+#             if i not in arr2:
+#                 l.append(i)
+#         l.sort()
+#         d = {}
+#         for i in arr2:
+#             d[i]=arr1.count(i)
+#         ans = []
+#         for i,j in d.items():
+#             a = [i]*j
+#             ans+=a
+#         return ans+l
