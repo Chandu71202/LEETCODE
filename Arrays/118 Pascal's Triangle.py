979199af64d709bf14889f900d4b51c3df4b53bd
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-10-15 21:41:55 +0530

    Create 118 Pascal's Triangle.py

---
+++ Arrays/118 Pascal's Triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows==1:
+            return [[1]]
+        
+        ans = [[1]*(i+1) for i in range(numRows)]
+        
+        for i in range(2,len(ans)):
+            for j in range(1,len(ans[i])-1):
+                ans[i][j] = ans[i-1][j-1]+ans[i-1][j]
+
+        return ans
