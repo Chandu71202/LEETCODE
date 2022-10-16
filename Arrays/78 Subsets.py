f6f5115a076dbb76f3c1a14f7c810e0af3c7a4ff
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-10-16 20:17:44 +0530

    Update 78 Subsets.py

--- Arrays/78 Subsets.py
+++ Arrays/78 Subsets.py
@@ -1,20 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def get_bit(num, bit):
-            temp = (1 << bit)
-            temp = temp & num
-            if temp == 0:
-              return 0
-            return 1
-
-        def get_all_subsets(v, sets):
-            subsets_count = 2 ** len(v)
-            for i in range(0, subsets_count):
-              st = []
-              for j in range(0, len(v)):
-                 if get_bit(i, j) == 1:
-                    st.append(v[j])
-              sets.append(st)
-        subsets = []
-        get_all_subsets(nums, subsets)
-        return subsets
+        res = []
+        
+        def backtrack(i,subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return 
+            
+            subset.append(nums[i])
+            backtrack(i+1,subset)
+            
+            subset.pop()
+            backtrack(i+1,subset)
+            
+            
+        backtrack(0,[])
+        return res
