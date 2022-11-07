334d71f741f0cff13c62188c4416bb2b6c28acc9
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-11-07 16:42:07 +0530

    Create 1323 Maximum 69 Number.py

---
+++ Random/1323 Maximum 69 Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        s = str(num)
+        for i in range(len(s)):
+            if s[i]=='6':
+                return int(s[:i]+'9'+s[i+1:])
+        return num 
